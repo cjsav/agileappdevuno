87208b3074ed707b1d0849c14eeefb5ffe12a458
Casey S <csavoi6@gmail.com>
2022-03-10 13:34:19 -0600

    code from module 1

---
+++ module1.py
@@ -0,0 +1,33 @@
+#Module 1
+#Casey Savoie
+
+#int
+num1 = 1
+
+print(num1)
+
+#float
+num2 = 1.1
+
+print(num2)
+
+#boolean
+status = True
+
+print(status)
+
+#string
+mystr = "Hello Class"
+
+print(mystr)
+
+#tuple
+mytup = (1,2,3,4)
+
+print(mytup)
+
+#dictionary
+mydict = {"key1":1, "key2":2, "key3":3}
+
+print(mydict)
+
